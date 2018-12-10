0f1e574e824051655ace15e432d9211d68551340
bodoi304 <hoangquanghuy1510@gmail.com>
2018-12-10 09:28:09 +0700

    a

---
+++ Controllers/tchqController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HQrefer;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+using static HQrefer.HQServiceSoapClient;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class tchqController : ControllerBase
+    {
+
+       
+        [HttpPost]
+        public ActionResult<string> TraCuuTTNopThue(postObject obj)
+        {
+            HQServiceSoapClient wSHQ = new HQServiceSoapClient(EndpointConfiguration.HQServiceSoap);
+            Task<TraCuuTTNopThueResponse> objRes= wSHQ.TraCuuTTNopThueAsync(obj.maDN, obj.soTK);
+            TraCuuTTNopThueResponse result = objRes.Result;
+            TraCuuTTNopThueResponseBody a = result.Body;
+            string b = a.TraCuuTTNopThueResult;
+            return b;
+        }
+
+        [HttpPost]
+        public ActionResult<string> TraCuuTTToKhaiHaiQuan(postObject obj)
+        {
+            HQServiceSoapClient wSHQ = new HQServiceSoapClient(EndpointConfiguration.HQServiceSoap);
+            Task<TraCuuTTToKhaiHaiQuanResponse> objRes= wSHQ.TraCuuTTToKhaiHaiQuanAsync(obj.maDN, obj.soTK,obj.cmnd);
+            TraCuuTTToKhaiHaiQuanResponse result = objRes.Result;
+            TraCuuTTToKhaiHaiQuanResponseBody a = result.Body;
+            string b = a.TraCuuTTToKhaiHaiQuanResult;
+            return b;
+        }
+    }
+}
---
+++ Model/postObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model {
+    public class postObject {
+        public string maDN { get; set; }
+        public string soTK { get; set; }
+        public string cmnd { get; set; }
+
+    }
+}
